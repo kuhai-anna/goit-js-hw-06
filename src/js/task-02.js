1ad6b01237d5cc028068242b5b7f766feef2c6e6
KuhaiAnna <kuhai.anna@gmail.com>
2023-02-11 23:05:54 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listEl = document.querySelector('#ingredients');
+
+const itemsEl = ingredients.map(ingredient => {
+	const ingredientEl = document.createElement('li');
+	ingredientEl.textContent = ingredient;
+	ingredientEl.classList.add('item');
+
+	return ingredientEl;
+});
+
+listEl.append(...itemsEl);
+
+console.log(listEl);
