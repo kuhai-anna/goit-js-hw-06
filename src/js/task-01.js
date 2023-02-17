5e0aa217607da6db1014e07fd17ab2053f60e52b
KuhaiAnna <kuhai.anna@gmail.com>
2023-02-17 20:15:07 +0200

    Update task-01.js
    Shorten the record

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,14 +1,13 @@
 const listEl = document.querySelector('#categories');
 
 const allCategories = [...listEl.children];
+
 console.log('Number of categories: ', allCategories.length);
 
 allCategories.forEach(category => {
-	const titleOfCategoryEl = category.firstElementChild;
-	console.log('Category: ', titleOfCategoryEl.textContent);
+	console.log('Category: ', category.firstElementChild.textContent);
 
 	const listOfCategoryElementsEl = category.lastElementChild;
 
-	const itemOfCategoryElementsEl = listOfCategoryElementsEl.children;
-	console.log('Elements: ', itemOfCategoryElementsEl.length);
+	console.log('Elements: ', listOfCategoryElementsEl.children.length);
 });
