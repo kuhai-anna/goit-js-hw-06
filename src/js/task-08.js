238b14ba718c7847bcdd708dac0edf5c1d7ec3d7
KuhaiAnna <kuhai.anna@gmail.com>
2023-02-20 00:48:18 +0200

    Update task-08.js
    Add the solution

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
+// 1 Отримати посилання на форму
+const formEl = document.querySelector('.login-form');
 
+// 2 Додати функцію обробки відправлення форми
+const handleSubmit = event => {
+	event.preventDefault();
+
+	const formElements = event.currentTarget.elements;
+	const email = formElements.email.value;
+	const password = formElements.password.value;
+
+	if (email === '' || password === '') {
+		alert('Please fill in all the fields!');
+	} else {
+		const result = {
+			email,
+			password,
+		};
+
+		console.log(result);
+		formEl.reset();
+	}
+};
+
+// 3 Додати слухачів подій
+formEl.addEventListener('submit', handleSubmit);
