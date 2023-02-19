c33e4892aad9f30fd26468360dafceef5760f5d6
KuhaiAnna <kuhai.anna@gmail.com>
2023-02-20 01:22:10 +0200

    Update task-09.js
    Add the solution

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,24 @@
+// function getRandomHexColor() {
+// 	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+// 1 Отримати посилання на елементи
+const bodyEl = document.querySelector('body');
+const textColorEl = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+// 2 Додати функцію, яка змінює колір фону
+const changeBodyColor = () => {
+	const color = getRandomHexColor();
+	bodyEl.style.backgroundColor = color;
+	textColorEl.textContent = color;
+};
+
+// 3 Додати слухачів подій
+changeColorBtn.addEventListener('click', changeBodyColor);
