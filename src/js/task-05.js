9b24e93a501929fab247f6612de8911a044332eb
KuhaiAnna <kuhai.anna@gmail.com>
2023-02-19 21:09:48 +0200

    Update task-05.js
    Add ternary operator

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,10 +2,9 @@ const inputEl = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 const handleEnter = event => {
-	if (event.currentTarget.value === '') {
-		return (output.textContent = 'Anonymous');
-	}
-	return (output.textContent = event.currentTarget.value);
+	event.currentTarget.value === ''
+		? (output.textContent = 'Anonymous')
+		: (output.textContent = event.currentTarget.value);
 };
 
 inputEl.addEventListener('input', handleEnter);
